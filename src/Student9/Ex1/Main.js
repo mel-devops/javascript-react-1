ce46a0065f89ecd7354658a6ef75ecae6c4d07d8
Melissa Schaefer <mschaefer@xpanxion.com>
2023-05-16 18:00:32 -0400

    update with Ex1

---
+++ src/Student9/Ex1/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Main.css'
+
+export const Main = () => {
+  return (
+    <div>
+        Dollar Food Store
+    </div>
+  )
+}
--- src/Student9/Ex1/Nav.js
+++ src/Student9/Ex1/Nav.js
@@ -3,8 +3,6 @@ import './Main.css'
 
 export const Main = () => {
   return (
-    <div>
-        Hello I'm student id: 1.
-    </div>
+    <div>  Home|Login|Suppot|FAQ    </div>
   )
 }
