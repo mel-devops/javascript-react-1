2e7d5f0828b4484de62367f7c2dcbd4f3e27d88b
Melissa Schaefer <mschaefer@xpanxion.com>
2023-05-18 15:00:06 -0400

    update with ex4

--- src/Student9/Ex3/Tweets.js
+++ src/Student9/Ex3/Tweets.js
@@ -6,7 +6,7 @@ export const Tweets = () => {
   return (
     <div>
         <div className="Main">
-            <div class="header"> Tweet Storm</div>
+            <div class="header"> Tweet Likes</div>
             <div class="green"> <Tweet  message="It's payday!!!" color ="green"/> </div>
             <div class="yellow"><Tweet className = "yellow" message="I just bought a new car!" /></div>
             <div class="red"><Tweet className = "red" message="I just smashed my new car." /></div>
